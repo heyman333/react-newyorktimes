45943eb989d1c6b8a4189909160f1523c25092b8
Youngsoo Han <amazingmobdev@gmail.com>
2019-03-01 23:33:44 +0900

    필수 패키지 설정

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+
+import * as articleActions from "./modules/article/actions";
+import { ArticleList } from "./containers";
 
 class App extends Component {
+  componentDidMount = () => {
+    const { AuthActions } = this.props;
+    AuthActions.fetchArticlesByPage(0);
+  };
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    const { articles } = this.props;
+    return <ArticleList list={articles} />;
   }
 }
 
-export default App;
+export default connect(
+  state => {
+    return {
+      articles: state.article.articles,
+    };
+  },
+  dispatch => ({
+    AuthActions: bindActionCreators(articleActions, dispatch),
+  })
+)(App);
